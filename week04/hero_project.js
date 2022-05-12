5b30b97a78941ad067309c726c302b9ad2fdee47
Ray Tang <43460657+bananaJuice2018@users.noreply.github.com>
2022-05-11 22:32:39 -0600

    Update hero_project.js

--- week04/hero_project.js
+++ week04/hero_project.js
@@ -3,16 +3,37 @@
 const form = document.forms["hero"];
 const input = form["heroName"];
 
-form.addEventListener("submit", heroDisplay, false);
+form.addEventListener("submit", makeHero, false);
 
-function heroDisplay(event) {
-    event.preventDefault(); // prevent the form from being submitted
+function makeHero(event) {
+  event.preventDefault(); // prevent the form from being submitted
 
-    const hero = {}; // create an empty object
+  const hero = {}; // create an empty object
 
-    hero.name = form["heroName"].value; // create a name property based on the input field's value
-    hero.realName = form["password"].value;
+  // username and password.
+  hero.name = form["heroName"].value; // create a name property based on the input field's value
+  hero.realName = form["realName"].value;
 
-    alert(JSON.stringify(hero)); // convert object to JSON string and display in alert dialog
-    return hero;
+  // Checkbox
+  hero.powers = [];
+  for (let i = 0; i < form.powers.length; i++) {
+    if (form.powers[i].checked) {
+      // If its checked, we add teh value to the array.
+      hero.powers.push(form.powers[i].value);
+    }
+  }
+
+  hero.powers = [...form.powers]
+    .filter((box) => box.checked)
+    .map((box) => box.value);
+
+  // Radio button.
+  hero.category = form["category"].value;
+
+  // Age
+  hero.age = form.age.value;
+
+  alert(JSON.stringify(hero)); // convert object to JSON string and display in alert dialog
+  return hero;
 }
+
